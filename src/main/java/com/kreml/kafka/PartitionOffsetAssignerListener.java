9ed07aac7acc6bd15463fa5b8190f7cfc8733f58
kreml <kremliovskyi@hotmail.com>
2019-11-26 18:25:27 +0200

    Кумукеув PartitionOffsetAssignerListener

--- src/main/java/com/kreml/kafka/PartitionOffsetAssignerListener.java
+++ src/main/java/com/kreml/kafka/PartitionOffsetAssignerListener.java
@@ -1,7 +1,7 @@
 package com.kreml.kafka;
 
-import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -13,31 +13,35 @@ public class PartitionOffsetAssignerListener implements ConsumerRebalanceListene
 
     private final Logger logger = LogManager.getLogger();
 
-    private Consumer consumer;
+    private KafkaConsumer consumer;
     private boolean shouldSeekToEnd;
-    private Collection<TopicPartition> revokedPartitions = new ArrayList<>();
+    private Collection<TopicPartition> assignedPartitions = new ArrayList<>();
 
-    public PartitionOffsetAssignerListener(Consumer kafkaConsumer, boolean shouldSeekToEnd) {
+    PartitionOffsetAssignerListener(KafkaConsumer kafkaConsumer, boolean shouldSeekToEnd) {
         this.consumer = kafkaConsumer;
         this.shouldSeekToEnd = shouldSeekToEnd;
     }
 
     @Override
     public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
-        logger.debug("Partitions are revoked: " + partitions);
-        revokedPartitions.addAll(partitions);
+
     }
 
     @Override
     public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
-        if (revokedPartitions.isEmpty()) {
-            if (shouldSeekToEnd) {
-                consumer.seekToEnd(partitions);
-            } else {
-                consumer.seekToBeginning(partitions);
-            }
+        if (assignedPartitions.isEmpty()) {
+            assignedPartitions.addAll(partitions);
         } else {
-            revokedPartitions.clear();
+            if (partitions.removeAll(assignedPartitions)) {
+                logger.warn("Duplicate partitions were reassigned.");
+            }
+            assignedPartitions.addAll(partitions);
         }
+        if (shouldSeekToEnd) {
+            consumer.seekToEnd(partitions);
+        } else {
+            consumer.seekToBeginning(partitions);
+        }
+
     }
 }
